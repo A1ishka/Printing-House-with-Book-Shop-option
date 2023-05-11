1517f3fce79764ff57efab6d383565c488a6ad97
A1ishka <100873237+A1ishka@users.noreply.github.com>
2023-05-12 00:12:59 +0300

    jwt edit

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,20 +1,22 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
+import cookieParser from "cookie-parser";
 
-export default (req, res, next) => {
-  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, 'printingBooks');
-      req.userId = decoded._id;
-      next();
-    } catch (verify_err) {
-      return res.status(403).json({
-        message: 'Для дальнейших действий необходимо войти в аккаунт',
-      });
+export default (req,res,next)=>{
+    const token = (req.cookies.jwt || '');
+    if (token){
+        try {
+            req.userId = returnID(token);
+        }catch (e){
+            return res.redirect("../");
+        }
+        next();
+    }else{
+        return res.redirect("../");
     }
-  } else {
-    return res.status(403).json({
-      message: 'Для дальнейших действий необходимо зарегистрироваться или войти в аккаунт',
-    });
-  }
-};
+}
+
+export const returnID = (token)=>{
+    const cleartoken = (token || '').replace(/Bearer\s?/,'')
+    const decoded = jwt.verify(cleartoken, 'printingBooks');
+    return decoded._id;
+}
